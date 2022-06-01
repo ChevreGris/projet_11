b1c639147d1aacc3e7067f0c7f1cb2f51dd0f3f5
mark.d <dumet.mark@gmail.com>
2022-06-01 17:42:49 +0200

    z

---
+++ tests/test_server.py
@@ -0,0 +1,14 @@
+class TestAuth:
+
+    def test_should_access_home(self, client):
+        assert client.get('/').status_code == 200
+
+    def test_should_redirect_known_email(self, client):
+        email = "john@simplylift.co"
+        response = client.post('/showSummary', data={'email' : email})
+        assert response.status_code == 200
+
+    def test_unknown_email_should_return_error(self, client):
+        email = 'adress@test.fr'
+        response = client.post('/showSummary', data={'email' : email})
+        assert response.status_code == 302
